eb9b77ccc170d45e69fcda815b57283d7eaa0fec
Bonface Maina <mainabonface4@gmail.com>
2023-04-23 11:35:33 +0300

    class for controller package

--- src/main/java/com/springboot/blog/Controller/PostController.java
+++ src/main/java/com/springboot/blog/Controller/PostController.java
@@ -4,10 +4,9 @@ import com.springboot.blog.Service.PostService;
 import com.springboot.blog.payload.PostDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -22,5 +21,15 @@ public class PostController {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
 
 
+    }
+    //get all posts rest api
+    @GetMapping
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPosts();
+
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
+        return ResponseEntity.ok(postService.getPostById(id));
     }
 }
--- src/main/java/com/springboot/blog/Service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/Service/impl/PostServiceImpl.java
@@ -1,11 +1,15 @@
 package com.springboot.blog.Service.impl;
 
 import com.springboot.blog.Service.PostService;
+import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.model.Post;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -16,18 +20,56 @@ public class PostServiceImpl implements PostService {
     }
     @Override
     public PostDto createPost(PostDto postDto){
+        //convert DTO to Entity
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+        //Convert entity to DTO
+        PostDto postResponse = mapToDto(newPost);
+        return postResponse;
+
         //convert DTO to entity
+//        Post post = new Post();
+//        post.setTitle((postDto.getTitle()));
+//        post.setDescription(postDto.getDescription());
+//        post.setContent(postDto.getContent());
+//        Post newPost =  postRepository.save(post);
+        //convert entity to dto
+//        PostDto postResponse = new PostDto();
+//        postResponse.setId(newPost.getId());
+//        postResponse.setTitle(newPost.getTitle());
+//        postResponse.setDescription(newPost.getDescription());
+//        postResponse.setContent(newPost.getContent());
+
+           }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+        //return null;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post","id",id));
+        return mapToDto(post);
+    }
+
+    private PostDto mapToDto(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+
+    }
+    private  Post mapToEntity(PostDto postDto){
         Post post = new Post();
-        post.setTitle((postDto.getTitle()));
+        post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
-        Post newPost =  postRepository.save(post);
-        //convert entity to dto
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
+        return post;
 
-        return postResponse;   }
+    }
 }
