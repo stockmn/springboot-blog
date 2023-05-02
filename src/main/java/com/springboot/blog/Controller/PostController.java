eb7ad7e5f7dfdb01b075fd341205f1b6aee13ca0
Bonface wambugu Maina <44479507+stockmn@users.noreply.github.com>
2023-05-02 17:09:44 +0300

    validations implementations

--- src/main/java/com/springboot/blog/Controller/PostController.java
+++ src/main/java/com/springboot/blog/Controller/PostController.java
@@ -4,6 +4,7 @@ import com.springboot.blog.Service.PostService;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class PostController {
     }
     //create blog post
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
 
 
@@ -42,7 +43,7 @@ public class PostController {
     }
     //update post by id
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,@PathVariable(name="id") long id){
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable(name="id") long id){
      PostDto postResponse =postService.updatePost(postDto,id);
      return new ResponseEntity<>(postResponse,HttpStatus.OK);
     }
--- src/main/java/com/springboot/blog/exception/GlobalExceptionsHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionsHandler.java
@@ -1,17 +1,24 @@
 package com.springboot.blog.exception;
 
 import com.springboot.blog.payload.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 
-public class GlobalExceptionsHandler {
+public class GlobalExceptionsHandler extends ResponseEntityExceptionHandler {
     //handle specific exceptions
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
@@ -35,6 +42,23 @@ public class GlobalExceptionsHandler {
 
     }
     //global exceptions
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String , String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName,message);
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+
+        //return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+
 
 
 
